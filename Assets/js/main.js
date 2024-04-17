d5d1e1d5fd02ba53f4eb45c8efe60efa9122a062
augustine-ugwu <augustinehills@yahoo.com>
2024-04-17 02:49:42 +0100

    Modified Css, implementing Bootstrap5

---
+++ Assets/js/main.js
@@ -0,0 +1,30 @@
+//File uploads
+const dropArea = document.querySelector(".drop_box"),
+  button = dropArea.querySelector("button"),
+  dragText = dropArea.querySelector("header"),
+  input = dropArea.querySelector("input");
+let file;
+var filename;
+
+button.onclick = () => {
+  input.click();
+};
+
+// Modal
+const exampleModal = document.getElementById("signup");
+if (exampleModal) {
+  exampleModal.addEventListener("show.bs.modal", (event) => {
+    // Button that triggered the modal
+    const button = event.relatedTarget;
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute("data-bs-whatever");
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector(".modal-title");
+    const modalBodyInput = exampleModal.querySelector(".modal-body input");
+
+    modalTitle.textContent = `Fill the form below to Sign up`;
+  });
+}
