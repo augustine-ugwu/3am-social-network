759311797b230be14ed70dc7529f38c4ccda00cd
augustine-ugwu <augustinehills@yahoo.com>
2024-04-19 05:48:54 +0100

    Modified Website

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -3,14 +3,27 @@ const dropArea = document.querySelector(".drop_box"),
   button = dropArea.querySelector("button"),
   dragText = dropArea.querySelector("header"),
   input = dropArea.querySelector("input");
-let file;
-var filename;
 
 button.onclick = () => {
   input.click();
 };
 
-// Modal
+input.addEventListener("change", function () {
+  file = this.files[0];
+  showFileName(file);
+});
+
+function showFileName(file) {
+  if (file) {
+    // Update the text to display the file name
+    dragText.textContent = `${file.name}`;
+  } else {
+    // Reset the text if no file is selected
+    dragText.textContent = "Select File here";
+  }
+}
+
+// Sign Up Modal
 const exampleModal = document.getElementById("signup");
 if (exampleModal) {
   exampleModal.addEventListener("show.bs.modal", (event) => {
@@ -26,5 +39,41 @@ if (exampleModal) {
     const modalBodyInput = exampleModal.querySelector(".modal-body input");
 
     modalTitle.textContent = `Fill the form below to Sign up`;
+  });
+}
+
+// Login Modal
+const loginModal = document.getElementById("login");
+if (loginModal) {
+  loginModal.addEventListener("show.bs.modal", (event) => {
+    // Button that triggered the modal
+    const button = event.relatedTarget;
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute("data-bs-whatever");
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = loginModal.querySelector(".modal-title");
+    // Optionally, you can update the modal title here
+    // modalTitle.textContent = `Login`;
+  });
+}
+
+// Create Post Modal
+const createPostModal = document.getElementById("createPost");
+if (createPostModal) {
+  createPostModal.addEventListener("show.bs.modal", (event) => {
+    // Button that triggered the modal
+    const button = event.relatedTarget;
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute("data-bs-whatever");
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = createPostModal.querySelector(".modal-title");
+    // Optionally, you can update the modal title here
+    // modalTitle.textContent = `Create Post`;
   });
 }
