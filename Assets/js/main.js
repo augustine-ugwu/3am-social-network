5f086ae284455254a26419f71c61bbfd2457744e
augustine-ugwu <augustinehills@yahoo.com>
2024-04-22 10:54:18 +0100

    Removed scss files and bootstrap from node modules

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,3 +1,20 @@
+//Read More Button
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
 //File uploads
 const dropArea = document.querySelector(".drop_box"),
   button = dropArea.querySelector("button"),
